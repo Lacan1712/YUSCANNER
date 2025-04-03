4a765f9c10ba78c607ad3dd7586ef86aef189dd8
rodrigo <lacan.xrl80@gmail.com>
2025-04-03 19:07:48 -0400

    criando comando para listar interfaces

--- src/cli/commands/list.rs
+++ src/cli/commands/list.rs
@@ -1,6 +1,7 @@
 // src/cli/commands/list.rs
 use crate::cli::command::Command;
 use clap::Args;
+use pnet::datalink;
 
 #[derive(Args)]
 pub struct ListArgs {
@@ -14,16 +15,41 @@ pub struct ListArgs {
 }
 
 impl Command for ListArgs {
-    fn execute(&self) {
-        println!("Comando list selecionado");
-        
-        if self.verbose {
-            println!("Modo verboso ativado");
-        }
+    fn execute(&self) {        
+
+        let all_interfaces = datalink::interfaces();
         
         match &self.interfaces {
-            Some(names) => println!("Filtrando pelas interfaces: {:?}", names),
-            None => println!("Listando todas as interfaces"),
+            Some(filter_names) => {
+                println!("Filtrando pelas interfaces: {:?}", filter_names);
+                
+                for interface in all_interfaces {
+                    if filter_names.contains(&interface.name) {
+                        self.print_interface_details(&interface);
+                    }
+                }
+            },
+            None => {
+                println!("Listando todas as interfaces:");
+                
+                for interface in all_interfaces {
+                    self.print_interface_details(&interface);
+                }
+            }
+        }
+    }
+}
+
+impl ListArgs {
+    /// Método auxiliar para imprimir detalhes da interface
+    fn print_interface_details(&self, interface: &datalink::NetworkInterface) {
+        if self.verbose {
+            println!("\nInterface: {}", interface.name);
+            println!("  MAC: {}", interface.mac.unwrap_or_default());
+            println!("  IPs: {:?}", interface.ips);
+            println!("  Descrição: {}", interface.description);
+        } else {
+            println!("- {}", interface.name);
         }
     }
 }
