66eb0708c91d60c4be2db75af01698efb7565314
rodrigo <lacan.xrl80@gmail.com>
2025-04-14 20:51:47 -0400

    refact: detectando a instalação da dependenvy npcap quando o OS for linux

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,21 @@
 mod cli;
 use clap::Parser;
 use cli::args::Args;
+use std::path::Path;
+
+
+fn check_npcap_installed() -> bool {
+    Path::new(r"C:\Windows\System32\Npcap").exists()
+}
 
 fn main() {
+    
+    if cfg!(target_os = "windows") && !check_npcap_installed() {
+        eprintln!("⚠️ O Npcap não está instalado no seu sistema.");
+        eprintln!("Por favor, instale-o em: https://npcap.com/#download");
+        std::process::exit(1);
+    }
+
     let args = Args::parse();
     args.command.execute();
 }
